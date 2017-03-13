b7ef9e4115fe3c6aaad65785450213b22e0270d6
GitHub Student <raghu_karamel@intuit.com>
2017-03-13 14:36:30 -0700

    Here is my first commit on my first Swift project

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet var tipCalcView: UIView!
+
+    @IBOutlet weak var tipControl2: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.2, 0.3]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * tipPercentages[tipControl2.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func changeTip(_ sender: Any) {
+        calculateTip(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let defaults=UserDefaults.standard
+        tipControl2.selectedSegmentIndex=defaults.integer(forKey: "defaultTipIndex")
+        calculateTip(self)
+        
+        tipCalcView.alpha = 0
+        UIView.animate(withDuration: 5, animations: {
+            self.tipCalcView.alpha=1
+        })
+        
+        billField.becomeFirstResponder()
+   }
+    
 }
 
